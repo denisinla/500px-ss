2bd04df9c82f6f28a6f8b2fe84e75fccad3eddb3
Denis Kabistan <dkabistan@gmail.com>
2013-10-23 21:19:00 -0700

    Refactoring js for new usage.

--- js/social-stack.js
+++ js/social-stack.js
@@ -1,27 +1,18 @@
 $(document).ready(function(){
 	var apiKey = "YnpJXu02wEwR6HVMuefDn76loEIY2O6KfoyEXKd0";
-	var url = "https://api.500px.com/v1/photos?consumer_key="+apiKey+"&feature=popular";
-	var objArray = [];
+	var apiLoad = "https://api.500px.com/v1/photos?consumer_key="+apiKey+"&feature=popular";
 	var siteUrl = "http://www.500px.com/";
-  $.getJSON(url, function(data){
-		$.each(data.photos, function(i, photo){
-					objArray.push(i+ ":" +photo);
-					/*objArray.push('<article class="_item">');
-					objArray.push('<a href='+siteUrl+''+photo.user.username+'>');
-					objArray.push('<img src='+photo.image_url+' alt='+photo.user.fullname+' />');
-					objArray.push('</a>');
-					objArray.push('<div class="description">');
-					objArray.push('<div class="twenty-two-pixels">')
-					objArray.push('<i class="twenty-two-pixels icon-camera-retro"></i>');
-					objArray.push('</div>');
-					objArray.push('<div class="user">');
-					objArray.push('<a href='+siteUrl+''+photo.user.username+'>'+photo.user.username+'</a>');
-					objArray.push('</div>');
-					objArray.push('</article>');
-					$('<div/>',{"class":"_col",html: objArray.join("")}).appendTo("#spread");
-					*/
-					$("#spread").append(objArray.join(''));
-					console.log("Set 1:"+ objArray);
-		});
+	$.ajax({
+		type: 'GET',
+		url: apiLoad,
+		datatype: 'json',
+		success: function(data){
+			$.each(data.photos, function(index, element){
+				$('<div/>')
+				.addClass('_col')
+				.append($('<img/>',{src:element.image_url}),$('<a/>',{href: 'http://500px.com/' + element.user.username,text:element.user.username}))
+				.appendTo('#spread');
+			});
+		}
 	});
 });
