5d8a5065485a584d083f9d25c6b7a043a24db982
denisinla <dkabistan@gmail.com>
2015-06-01 23:56:19 -0700

    Update: Express 4 update.

--- server/web.js
+++ server/web.js
@@ -1,31 +1,9 @@
-// DEPENDENCIES
-// ============
-var express = require("express"),
-    http = require("http"),
-    port = (process.env.PORT || 3000),
-    server = module.exports = express();
+var express = require('express');
+var app = express();
 
-// SERVER CONFIGURATION
-// ====================
-server.configure(function () {
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/../public'));
 
-    server.use(express["static"](__dirname + "/../public"));
-
-    server.use(express.errorHandler({
-
-        dumpExceptions:true,
-
-        showStack:true
-
-    }));
-
-    server.use(server.router);
+app.listen(app.get('port'), function() {
+  console.log("Express running on localhost:" + app.get('port'));
 });
-
-// SERVER
-// ======
-
-// Start Node.js Server
-http.createServer(server).listen(port);
-
-console.log('Node server loaded! Happy Hacking!');
